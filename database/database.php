c0c7ec6b8d040a89952b87a006d29edc8bfbdec0
Francesco Maurella <francesco.maurella@live.it>
2021-03-02 16:52:36 +0100

    Compilazione dati

--- database/database.php
+++ database/database.php
@@ -2,11 +2,63 @@
 
 $dischi = [
     [
-        'dettaglio 1',
-        'dettaglio 2'
+        'title' => 'New Jersey',
+        'artist' => 'Bon Jovi',
+        'cover' => __DIR__ . '/img/01.jpg',
+        'year' => 1988
     ],
     [
-        'dettaglio 1',
-        'dettaglio 2'
+        'title' => 'Live at Wembley 86',
+        'artist' => 'Queen',
+        'cover' => __DIR__ . '/img/02.jpg',
+        'year' => 1992
+    ],
+    [
+        'title' => 'Ten\'s Summoner\'s Tales',
+        'artist' => 'Sting',
+        'cover' => __DIR__ . '/img/03.jpg',
+        'year' => 1993
+    ],
+    [
+        'title' => 'Steve Gadd Band',
+        'artist' => 'Steve Gadd Band',
+        'cover' => __DIR__ . '/img/04.jpg',
+        'year' => 1988
+    ],
+    [
+        'title' => 'Brave New World',
+        'artist' => 'Iron Maiden',
+        'cover' => __DIR__ . '/img/05.jpg',
+        'year' => 2000
+    ],
+    [
+        'title' => 'One more car, one more raider',
+        'artist' => 'Eric Clapton',
+        'cover' => __DIR__ . '/img/06.jpg',
+        'year' => 2002
+    ],
+    [
+        'title' => 'Made in Japan',
+        'artist' => 'Deep Purple',
+        'cover' => __DIR__ . '/img/07.jpg',
+        'year' => 1972
+    ],
+    [
+        'title' => 'And Justice for all',
+        'artist' => 'Metallica',
+        'cover' => __DIR__ . '/img/08.jpg',
+        'year' => 1988
+    ],
+    [
+        'title' => 'Hard wired',
+        'artist' => 'Dave Weckl',
+        'cover' => __DIR__ . '/img/09.jpg',
+        'year' => 1994
+    ],
+    [
+        'title' => 'Bad',
+        'artist' => 'Michael Jackson',
+        'cover' => __DIR__ . '/img/10.jpg',
+        'year' => 1987
     ],
 ];
