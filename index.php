6838182185f7bb7db3dff631dce51d51c9af6085
Francesco Maurella <francesco.maurella@live.it>
2021-03-03 19:29:09 +0100

    Passaggio Css in Scss. FINE

--- index.php
+++ index.php
@@ -7,37 +7,48 @@ require_once __DIR__ . '/database/database.php';
   <head>
     <meta charset="utf-8">
     <title>JAVASCRIPT - PHP / AJAX [Dischi - Milestone#1]</title>
-    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
 
-    <!-- TOP-BAR HEADER -->
-    <header>
-      <div class="container al-cntr flx">
-        <img src="database/img/00-logo.png" alt="logo">
-      </div>
-    </header>
-    <!-- / TOP-BAR HEADER -->
+    <div class="wrapper">
 
-    <!-- MAIN DISC-LIST CONTENT -->
-    <main class="cds-container flx container">
-      <!-- Disc element -->
-      <?php
-      foreach ($discs as $disc) {
-        ?>
-        <div class="cd al-cntr flx">
-          <img src="<?php echo $disc['poster'] ?>" alt="poster">
-          <h3><?php echo $disc['title'] ?></h3>
-          <span class="author"><?php echo $disc['author'] ?></span>
-          <span class="year"><?php echo $disc['year'] ?></span>
+      <!-- TOP-BAR HEADER -->
+      <header class="top-bar">
+        <div class="container">
+          <img src="img/00-logo.png" alt="logo">
         </div>
-        <?php
-      }
-       ?>
-      <!-- / Disc element -->
-    </main>
-    <!-- / MAIN DISC-LIST CONTENT -->
+      </header>
+      <!-- / TOP-BAR HEADER -->
+
+      <!-- MAIN DISC-LIST CONTENT -->
+      <main class="main">
+        <div class="container">
+          <ul class="main-cds-list">
+            <!-- Disc element -->
+            <?php
+            foreach ($discs as $disc) {
+              ?>
+              <li class="main-cds-list-elm">
+                <div class="title">
+                  <img src="<?php echo $disc['poster'] ?>" alt="poster">
+                  <div><?php echo $disc['title'] ?></div>
+                </div>
+                <div class="data">
+                  <div><?php echo $disc['artist'] ?></div>
+                  <div><?php echo $disc['year'] ?></div>
+                </div>
+              </li>
+              <?php
+            }
+            ?>
+            <!-- / Disc element -->
+          </ul>
+        </div>
+      </main>
+      <!-- / MAIN DISC-LIST CONTENT -->
+      
+    </div>
 
   </body>
 </html>
